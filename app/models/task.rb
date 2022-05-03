59c349a8906f0c59969eddbc9dd7f223553576aa
Amit Uttam <changingrainbows@gmail.com>
2022-05-02 22:07:12 -0300

    Chapter 5 (Turbo Streams)

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,13 @@ class Task < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # Note: There are asynch alternatives to these methods e.g. #broadcast_prepend_later_to
+  # #broadcast_remove_later_to doesn't exist since once deleted, there's nothing left to retrieve, and start asynchronously.
+  # after_create_commit -> { broadcast_prepend_to "tasks", partial: "tasks/task", locals: { task: self }, target: "tasks" }
+  # after_update_commit -> { broadcast_replace_to "tasks", partial: "tasks/task", locals: { task: self }, target: "tasks" }
+  # after_destroy_commit -> { broadcast_remove_to "tasks", partial: "tasks/task", locals: { task: self }, target: "tasks" }
+
+  # Equivalent to above
+  broadcasts_to ->(task) { "tasks" }, inserts_by: :prepend
 end
